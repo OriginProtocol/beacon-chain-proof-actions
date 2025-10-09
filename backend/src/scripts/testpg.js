bf140766e97fb3697a86665bd399786e492297a5
Domen Grabec <grabec@gmail.com>
2025-10-09 17:44:16 +0200

    remove pglite

--- backend/src/scripts/testpg.js
+++
@@ -1,5 +0,0 @@
-const { PGlite } = require('@electric-sql/pglite');
-const path = require('path');
-const db = new PGlite(path.join(__dirname, 'pgdata'));
-db.query('SELECT 1').then(() => console.log('Success')).catch(err => console.error(err));
-
