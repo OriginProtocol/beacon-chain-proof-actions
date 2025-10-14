e48ca11bdae50e4e5d9bf9d4841a25acd8f39f01
Domen Grabec <grabec@gmail.com>
2025-10-14 16:44:17 +0200

    unify postgres names

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,12 +5,12 @@ const path = require('path');
 const env = process.env.NODE_ENV || 'development'; // Default to development
 
 const db = new Pool({
-  host: process.env.PG_HOST,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
-  port: process.env.PG_PORT || 5432,
-  ssl: process.env.PG_SSL === 'true' ? { rejectUnauthorized: false } : false, // Optional SSL for hosted DBs
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT || 5432,
+  ssl: process.env.PGSSL === 'true' ? { rejectUnauthorized: false } : false, // Optional SSL for hosted DBs
 });
 console.log('Connected to production PostgreSQL');
 
